f1fee24e6fd14c53acea6f89c563e03bb0b93f0f
Chad Bell <36672928+chadbennett16@users.noreply.github.com>
2018-08-03 23:48:57 -0400

    Initial commit

---
+++ DnD_Assistant.py
@@ -0,0 +1,15 @@
+"""
+Chad Bell
+08/04/2018
+DnD Assistant
+
+Goals:
+Be able to roll any specified die
+Keep track of PC information including:
+    > Stats
+    > Skills
+    > Inventory
+Save/Pull NPC data
+Setup Encounters
+Save progress
+"""
