139a67254d058666215c0b56b16e2dd56fa70cf6
Chad Bell <36672928+chadbennett16@users.noreply.github.com>
2018-08-06 16:33:52 -0400

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,4 +1,7 @@
+import xlrd * # Need to install xlrd
+
 class Player(object):
-    """description of class"""
-
-
+    """Represents a player's character sheet"""
+    #file_name = 'path to file'
+    def __init__(self, name):
+        self.name = "name"
