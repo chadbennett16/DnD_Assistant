fecd13a031c2b8f44c5bb443f877029d3558ca3e
Chad Bell <36672928+chadbennett16@users.noreply.github.com>
2018-08-06 15:52:28 -0400

    Added dice roll function

--- Dice.py
+++ Dice.py
@@ -1,4 +1,12 @@
+from random import randint
+
 class Dice(object):
     """description of class"""
+    def __init__(self, sides):
+        self.sides = "sides"
 
-
+    def dice_roll(self, sides):
+        return randint(1, sides)
+    
+    # For percentile, call dice_roll(1,10) * 10
+    
